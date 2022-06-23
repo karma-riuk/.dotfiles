8707d4c3e66641b77bd19ad7f20d6195d0ccc48e
Karma Riuk <riukkarma@gmail.com>
2022-06-23 17:20:21 +0200

    nvim - treesitter: added motions for functions

--- nvim/.config/nvim/plugin/treesitter/treesitter.lua
+++ nvim/.config/nvim/plugin/treesitter/treesitter.lua
@@ -1,5 +1,21 @@
 require'nvim-treesitter.configs'.setup {
     context_commentstring = {enable = true},
     highlight = {enable = true, disable = {"latex"}},
-    indent = {enable = true, disable = {"c", "cpp"}}
+    indent = {enable = true, disable = {"c", "cpp"}},
+    textobjects = {
+        select = {
+            enable = true,
+
+            -- Automatically jump forward to textobj, similar to targets.vim
+            lookahead = true,
+
+            keymaps = {
+                -- You can use the capture groups defined in textobjects.scm
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                ["ic"] = "@class.inner"
+            }
+        }
+    }
 }
--- nvim/.config/nvim/plugin/treesitter/treesitter.vim
+++ nvim/.config/nvim/plugin/treesitter/treesitter.vim
@@ -1,2 +1,3 @@
 set foldmethod=expr
 set foldexpr=nvim_treesitter#foldexpr()
+set nofoldenable
