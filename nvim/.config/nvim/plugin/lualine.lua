b108ae192e7d93e6dc1734515891e94440b63408
Karma Riuk <riukkarma@gmail.com>
2022-07-18 18:46:26 +0200

    nvim - lualine: added config for lualine bar

---
+++ nvim/.config/nvim/plugin/lualine.lua
@@ -0,0 +1,12 @@
+require('lualine').setup {
+    options = {
+        theme = "auto",
+        component_separators = {left = '|', right = '|'},
+        section_separators = {left = '', right = ''},
+        globalstatus = true,
+    },
+    extensions = {
+        "toggleterm",
+        "man"
+    }
+}
