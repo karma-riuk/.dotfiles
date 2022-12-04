0a236417b07880f674ec869c845338877097257f
Karma Riuk <riukkarma@gmail.com>
2022-12-04 13:40:29 +0100

    awesomewm: added floating rules

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -603,7 +603,7 @@ awful.rules.rules = {
                 "Arandr", "Blueman-manager", "Gpick", "Kruler", "MessageWin", -- kalarm.
                 "Sxiv", "Timeshift", "Tor Browser", -- Needs a fixed window size to avoid fingerprinting by screen size.
                 "Wpa_gui", "veromix", "Nm-connection-editor", "xtightvncviewer",
-                "gksqt"
+                "gksqt", "feh", "GeoGebra to PGF Export", "mpv"
             },
 
             -- Note that the name property shown in xprop might be set slightly after creation of the client
