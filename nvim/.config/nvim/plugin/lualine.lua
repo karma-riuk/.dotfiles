cd6a7f79876894d13395d2a4994940773c338718
Karma Riuk <riukkarma@gmail.com>
2023-03-24 14:05:10 +0100

    nvim - lualine: formatted code

--- nvim/.config/nvim/plugin/lualine.lua
+++ nvim/.config/nvim/plugin/lualine.lua
@@ -1,12 +1,12 @@
-require('lualine').setup {
+require("lualine").setup({
     options = {
         theme = "auto",
-        component_separators = {left = '|', right = '|'},
-        section_separators = {left = '', right = ''},
+        component_separators = { left = "|", right = "|" },
+        section_separators = { left = "", right = "" },
         globalstatus = true,
     },
     extensions = {
         "toggleterm",
-        "man"
-    }
-}
+        "man",
+    },
+})
