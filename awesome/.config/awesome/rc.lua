bdfe2b337d49b2fe3129fdfc715a335ac8908d40
Karma Riuk <riukkarma@gmail.com>
2023-06-29 19:06:44 +0200

    awesomewm: removed useless code

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -691,9 +691,8 @@ awful.rules.rules = {
             },
         },
         properties = { floating = true },
-        callback = function(c) end,
-    }, -- Set Firefox to always map on the tag named "2" on screen 1.
-    { rule = { class = "Firefox" }, properties = { screen = 1, tag = "2" } },
+        -- callback = function(c) end,
+    },
 }
 -- }}}
 
