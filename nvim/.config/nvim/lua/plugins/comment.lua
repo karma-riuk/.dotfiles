28b47c36cb2e87f46d92dac6a1ac02e19f8cc54a
Arnaud Fauconnet <riukkarma@gmail.com>
2024-06-07 23:10:56 +0200

    nvim - comment: fixed lil bug

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -1,16 +1,14 @@
 return {
     "numToStr/Comment.nvim",
     dependencies = "JoosepAlviste/nvim-ts-context-commentstring",
+    lazy = false,
     config = function()
         require("Comment").setup({
             pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
             toggler = {
-                line = "<C-_>",
+                line = "<C-/>",
             },
         })
     end,
-    keys = {
-        "<C-_>",
-        { "gc", mode = "v" },
-    },
+    keys = { "<C-_>", { "gc", mode = "v" } },
 }
