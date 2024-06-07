ec52025f0b375380113bc0aa3df294a85ad5b3e7
Arnaud Fauconnet <riukkarma@gmail.com>
2024-06-07 23:15:49 +0200

    nvim - vimtex: updated some stuff

--- nvim/.config/nvim/lua/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/plugins/vimtex.lua
@@ -3,32 +3,35 @@ return {
     ft = "tex",
     init = function()
         vim.g.vimtex_quickfix_ignore_filters = {
-            "\v(Over|Under)full",
-            "\vunder(bar|line)",
+            "\\v(Over|Under)full",
+            "\\vunder(bar|line)",
             "uop",
-            "\v.*[Ll]istings.*cpp.*",
+            "\\v.*[Ll]istings.*cpp.*",
             "Package Listings Error: Couldn't load requested language.",
         }
 
         vim.g.vimtex_view_method = "zathura"
 
         vim.g.vimtex_delim_toggle_mod_list = {
-            { "left", "\right" },
-            { "\bigl", "\bigr" },
-            { "Bigl", "Bigr" },
-            { "\biggl", "\biggr" },
-            { "Biggl", "Biggr" },
+            { "\\left", "\\right" },
+            { "\\bigl", "\\bigr" },
+            { "\\Bigl", "\\Bigr" },
+            { "\\biggl", "\\biggr" },
+            { "\\Biggl", "\\Biggr" },
         }
 
-        vim.g.vimtex_quickfix_autoclose_after_keystrokes = 2
+        vim.g.vimtex_quickfix_open_on_warning = 0
+
+        -- vim.g.vimtex_quickfix_autoclose_after_keystrokes = 2
+        local augroup = vim.api.nvim_create_augroup("vimtex_config", {})
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "VimtexEventQuit",
+            group = augroup,
+            desc = "Clean files on exit.",
+            command = [[ call vimtex#compiler#clean(0) ]],
+        })
 
         vim.cmd([[
-            augroup MyVimtex
-                autocmd!
-                " autocmd User VimtexEventQuit call vimtex#latexmk#clean(0)
-                autocmd User VimtexEventQuit VimtexClean
-            augroup END
-
             " augroup TexDollarPairs
             "     au!
             "     autocmd FileType tex let b:AutoPairs = AutoPairsDefine({'$': '$', "$$": "$$"})
