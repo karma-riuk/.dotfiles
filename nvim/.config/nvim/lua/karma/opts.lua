f153cdfa48a57e673ffc3ac0071c96b32deeb6ba
Arnaud Fauconnet <riukkarma@gmail.com>
2025-03-26 13:21:21 +0100

    nvim - opts: made the column width 100 instead of 80

--- nvim/.config/nvim/lua/karma/opts.lua
+++ nvim/.config/nvim/lua/karma/opts.lua
@@ -15,8 +15,8 @@ vim.opt.undofile = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.opt.colorcolumn = "80"
-vim.opt.textwidth = 80
+vim.opt.colorcolumn = "100"
+vim.opt.textwidth = 100
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
